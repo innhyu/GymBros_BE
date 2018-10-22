402af4e1671a2fc2d7fb2ebf3673c3750294ad5d
innhyu <innhyu@gmail.com>
2018-10-22 15:19:12 -0400

    relationships for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
 	#Used for role? method and sometimes views
 	ROLES = [['Real', :real]]
 
+	#Relationships
+	has_many :workouts
+	has_many :joined_workouts
+
 	#Use this to see if user exists in database, used in ability.rb
 	def role?(authorized_role)
 		return false if role.nil?
