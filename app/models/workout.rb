bddb5ce004a5e656c15d4f1f0d3acf089dbcea1b
innhyu <innhyu@gmail.com>
2018-10-23 15:22:39 -0400

    Fixing problem with message validators; touch this later

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -8,10 +8,10 @@ class Workout < ApplicationRecord
 	belongs_to :user
 
 	# Validations for fields
-	validates :title, presence: true, length: { minimum: 4 }, message: "Title must be longer than 4 characters"
-	validates_date :time, :on_or_after => lambda { Date.now + 1.hours }, message: "Time is not in a valid format"
+	validates :title, presence: true, length: { minimum: 4 }
+	validates_date :time, :on_or_after => lambda { Date.now + 1.hours }
 	validates_numericality_of :duration, only_integer: true, greater_than_or_equal_to: 30, less_than: 300
-	validates :location, presence: true, inclusion: { in: LOCATIONS }, message: "Location not supported"
+	validates :location, presence: true, inclusion: { in: LOCATIONS }
 	validates_numericality_of :team_size, only_integer: true, less_than: 10, greater_than_or_equal_to: 2
 	validates :finalized, presence: true
 
