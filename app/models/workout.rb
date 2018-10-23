a51080d0379512c59e391f17b33d4c4ffc4eaf66
innhyu <innhyu@gmail.com>
2018-10-23 15:17:50 -0400

    Finished validations for workout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,18 +1,18 @@
 class Workout < ApplicationRecord
-	belongs_to :user
 
 	# List of acceptable locations
 	LOCATIONS = ['WIEGAND GYMNASIUM', 'JARED L. COHON CENTER GYMNASIUM', 'SKIBO GYMNASIUM']
 
 	# Relationship
-	has_many :JoinedWorkouts
+	has_many :joined_workouts
+	belongs_to :user
 
 	# Validations for fields
-	validates :title, presence: true, length: { minimum: 4 }
-	validates :time, presence: true
-	validates :duration, presence: true
-	validates :location, presence: true, inclusion: { in: LOCATIONS }
-	validates :team_size, presence: true
+	validates :title, presence: true, length: { minimum: 4 }, message: "Title must be longer than 4 characters"
+	validates_date :time, :on_or_after => lambda { Date.now + 1.hours }, message: "Time is not in a valid format"
+	validates_numericality_of :duration, only_integer: true, greater_than_or_equal_to: 30, less_than: 300
+	validates :location, presence: true, inclusion: { in: LOCATIONS }, message: "Location not supported"
+	validates_numericality_of :team_size, only_integer: true, less_than: 10, greater_than_or_equal_to: 2
 	validates :finalized, presence: true
 
 	
