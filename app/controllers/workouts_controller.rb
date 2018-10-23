52fe534ee3c92681ad80b47945f55b4b80fc8dd4
innhyu <innhyu@gmail.com>
2018-10-23 15:00:13 -0400

    Workout swagger docs have been integrated

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,45 +1,45 @@
 class WorkoutsController < ApplicationController
-		# Documentation for User Controller
-		swagger_controller :workout, "Workout"
+	# Documentation for User Controller
+	swagger_controller :workout, "Workout"
 
-		swagger_api :index do
-			summary "Fetches all workouts"
-			notes "There is no visibility settings at the moment"
-		end
-	
-		swagger_api :show do 
-			summary "Shows a particular workout's data"
-			param :path, :id, :integer, :required, "Workout ID"
-			notes "This fetches all data related to the workout"
-			response :not_found
-		end
-	
-		swagger_api :create do
-			summary "Creates a workout with a given set of parameters"
-			param :form, :title, :integer, :optional, "Title"
-			param :form, :time, :string, :required, "Time"
-			param :form, :duration, :string, :required, "Duration"
-			param :form, :location, :string, :required, "Location"
-			param :form, :team_size, :string, :required, "Team Size"
-			response :not_acceptable
-		end
-	
-		swagger_api :update do
-			summary "Updates a workout with a given set of parameters"
-			param :form, :title, :integer, :optional, "Title"
-			param :form, :time, :string, :required, "Time"
-			param :form, :duration, :string, :required, "Duration"
-			param :form, :location, :string, :required, "Location"
-			param :form, :team_size, :string, :required, "Team Size"
-			response :not_acceptable
-			response :not_found
-		end
-	
-		swagger_api :destroy do
-			summary "Destroys a workout"
-			param :path, :id, :integer, :required, "Workout ID"
-			response :not_found
-		end
+	swagger_api :index do
+		summary "Fetches all workouts"
+		notes "There is no visibility settings at the moment"
+	end
+
+	swagger_api :show do 
+		summary "Shows a particular workout's data"
+		param :path, :id, :integer, :required, "Workout ID"
+		notes "This fetches all data related to the workout"
+		response :not_found
+	end
+
+	swagger_api :create do
+		summary "Creates a workout with a given set of parameters"
+		param :form, :title, :integer, :optional, "Title"
+		param :form, :time, :string, :required, "Time"
+		param :form, :duration, :string, :required, "Duration"
+		param :form, :location, :string, :required, "Location"
+		param :form, :team_size, :string, :required, "Team Size"
+		response :not_acceptable
+	end
+
+	swagger_api :update do
+		summary "Updates a workout with a given set of parameters"
+		param :form, :title, :integer, :optional, "Title"
+		param :form, :time, :string, :required, "Time"
+		param :form, :duration, :string, :required, "Duration"
+		param :form, :location, :string, :required, "Location"
+		param :form, :team_size, :string, :required, "Team Size"
+		response :not_acceptable
+		response :not_found
+	end
+
+	swagger_api :destroy do
+		summary "Destroys a workout"
+		param :path, :id, :integer, :required, "Workout ID"
+		response :not_found
+	end
 	
 	# Callbacks
 	before_action :set_workout, only: [:show, :update, :destroy]
