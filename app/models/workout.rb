6372bd491c992d98eb1cf7b9da763a19feb8260f
innhyu <innhyu@gmail.com>
2018-10-23 10:56:07 -0400

    Working on validations for workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,19 @@
 class Workout < ApplicationRecord
 	belongs_to :user
+
+	# List of acceptable locations
+	LOCATIONS = ['WIEGAND GYMNASIUM', 'JARED L. COHON CENTER GYMNASIUM', 'SKIBO GYMNASIUM']
+
+	# Relationship
+	has_many :JoinedWorkouts
+
+	# Validations for fields
+	validates :title, presence: true, length: { minimum: 4 }
+	validates :time, presence: true
+	validates :duration, presence: true
+	validates :location, presence: true, inclusion: { in: LOCATIONS }
+	validates :team_size, presence: true
+	validates :finalized, presence: true
+
+	
 end
