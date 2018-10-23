08796e91276226d6231704f96dfcc28f5d530d76
innhyu <innhyu@gmail.com>
2018-10-23 17:48:05 -0400

    Updating rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+	# Rails basic resources
 	resources :users
 	resources :workouts
 	resources :joined_workouts
-	#write custom routes for custom requests for joined_workouts
+
+	# Custom Routes
+	post 'joined_workouts/approve' => 'joined_workout#approve'
+ 	post 'joined_workouts/accept' => 'joined_workout#accept'
+
 end
