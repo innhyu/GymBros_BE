6183dcd3c96c07ce969c7d207cf180a666cb5400
innhyu <innhyu@gmail.com>
2018-10-23 16:21:54 -0400

    Fixing problem with DateTime and Date

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -9,11 +9,9 @@ class Workout < ApplicationRecord
 
 	# Validations for fields
 	validates :title, presence: true, length: { minimum: 4 }
-	validates_date :time, :on_or_after => lambda { Date.now + 1.hours }
+	validates_datetime :time, :on_or_after => lambda { DateTime.now.utc + 1.hours }
 	validates_numericality_of :duration, only_integer: true, greater_than_or_equal_to: 30, less_than: 300
 	validates :location, presence: true, inclusion: { in: LOCATIONS }
 	validates_numericality_of :team_size, only_integer: true, less_than: 10, greater_than_or_equal_to: 2
-	validates :finalized, presence: true
-
 	
 end
