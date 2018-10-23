725657c2f8d785d9058a70cb08b8f5d9d7da767f
innhyu <innhyu@gmail.com>
2018-10-23 16:55:15 -0400

    Changing some specifications to have accept and apporve instead of update endpoint

--- app/controllers/joined_workouts_controller.rb
+++ app/controllers/joined_workouts_controller.rb
@@ -1,4 +1,29 @@
 class JoinedWorkoutsController < ApplicationController
+	# Documentation for User Controller
+	swagger_controller :joined_workouts, "Joined Workout"
+
+	swagger_api :create do
+		summary "Joins a user into a workout through an extra table"
+		# TODO : ADD IN AUTHENTICATION THEN USE THE USER ID
+		param :form, :user_id, :integer, :required, 'User ID'
+		param :form, :workout_id, :integer, :required, 'Workout ID'
+		response :not_acceptable
+	end
+
+	swagger_api :accept do 
+
+	end
+
+	swagger_api :approve do
+
+	end
+
+	swagger_api :destroy do
+		summary "Destroys a user with a given set of parameters"
+		param :path, :id, :integer, :required, "User ID"
+		response :not_found
+	end
+
 	# Callbacks
 	before_action :set_joined_workout, only: [:update, :destroy]
 
@@ -15,11 +40,22 @@ class JoinedWorkoutsController < ApplicationController
 		end
 	end
 
-	# An endpoint to update joined workout
-	# Parameters: joined_workout params
-	# TODO : Use authentication to set the user_id
-	def update
-		if @joined_workout.update(joined_workout_params)
+	# An endpoint for the host to accept a user into a workout
+	# Parameters: joined_workout id
+	# TODO : Use authentication to check that the owner is calling this
+	def accept
+		if @joined_workout.update(accept: true)
+			render json: @joined_workout
+		else
+			render json: @joined_workout.errors, status: :unprocessable_entity
+		end
+	end
+
+	# An endpoint for a user to approve a change in the workout
+	# Parameters: joined_workout id  
+	# TODO : Use authentication to check that 
+	def approve 
+		if @joined_workout.update(approved: true)
 			render json: @joined_workout
 		else
 			render json: @joined_workout.errors, status: :unprocessable_entity
@@ -39,8 +75,4 @@ class JoinedWorkoutsController < ApplicationController
 		@joined_workout.JoinedWorkout.find(params[:id])
 	end
 
-	# Permitted parameters for the joined_workout
-	def joined_workout_params
-		params.permit(:approved, :checked_in)
-	end
 end
