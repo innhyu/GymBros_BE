b2dc0b2136428ed36adb54c52ee976bb6c24ff9e
Dylan Schwartz <dylanschwartz@Dylans-MacBook-Pro.local>
2018-10-23 22:41:39 -0400

    changed db to psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181021195054) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "joined_workouts", force: :cascade do |t|
     t.integer "workout_id"
     t.integer "user_id"
