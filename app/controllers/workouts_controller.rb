cb2f565501ad8956f4b88928cbf3a7d88640ed52
Dylan Schwartz <dylanschwartz@Dylans-MacBook-Pro.local>
2018-12-01 22:49:17 -0500

    updated workout show action again

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -73,14 +73,24 @@ class WorkoutsController < ApplicationController
 	# Note: Returns all related information such as all joined_workouts and 
 	def show
 		unless params[:user_id]
+			print("hi1")
 			@joined_workouts = @workout.joined_workouts
 			render :json => {:workout => @workout, :joined_workouts => @joined_workouts.map {|jw| [jw, jw.user] }, :owner => @workout.user}
 		else
-			if params[:user_id] == @workout.user.id
+			#owner call
+			if params[:user_id].to_i == @workout.user.id
 				@joined_workouts = @workout.joined_workouts
+				print("hi2")
 				render :json => {:workout => @workout, :joined_workouts => @joined_workouts.map {|jw| [jw, jw.user] }, :owner => @workout.user}
-			else
+			#accepted user call
+			elsif @workout.joined_workouts.accepted_users.map{|jw| jw.user.id }.include?(params[:user_id].to_i)
+				print("hi3")
 				@joined_workouts = @workout.joined_workouts.accepted_users
+				render :json => {:workout => @workout, :joined_workouts => @joined_workouts.map {|jw| [jw, jw.user] }, :owner => @workout.user}
+			#stranger call
+			else
+				@joined_workouts = @workout.joined_workouts.where(workout_id: @workout.id, user_id: @workout.user.id)
+				print("hello")
 				render :json => {:workout => @workout, :joined_workouts => @joined_workouts.map {|jw| [jw, jw.user] }, :owner => @workout.user}
 			end
 		end
